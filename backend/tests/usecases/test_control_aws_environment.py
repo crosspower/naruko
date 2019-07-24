b0a0197489896c2c2255561ea0d2b4b2d3115703
shoichi-sasaki <shoichi-sasaki@x-power.co.jp>
2019-07-24 21:23:22 +0900

    リクエスト修正

--- backend/tests/usecases/test_control_aws_environment.py
+++ backend/tests/usecases/test_control_aws_environment.py
@@ -2,6 +2,7 @@ from django.core.exceptions import PermissionDenied
 from django.test import TestCase
 from unittest.mock import Mock, patch
 from unittest import mock
+import datetime
 # デコレーターをmock化
 with patch('backend.models.OperationLogModel.operation_log', lambda executor_index=None, target_method=None, target_arg_index_list=None: lambda func: func):
     from backend.usecases.control_aws_environment import ControlAwsEnvironment
@@ -164,34 +165,134 @@ class ControlAwsEnvironmentTestCase(TestCase):
     # 請求情報取得：正常系
     @mock.patch('backend.usecases.control_aws_environment.CloudWatch')
     def test_billing_graph(self, mock_cloudwatch: Mock):
+        # テスト用の引数準備
+        external_result = [{
+                'timestamps': [datetime.datetime(2019, 4, 29, 12, 0), datetime.datetime(2019, 4, 30, 0, 0)],
+                'values': [446.11, 458.22],
+                'config': {
+                    'metric_name': 'EstimatedCharges',
+                    'dimensions': [{'Name': 'ServiceName', 'Value': 'AmazonCloudWatch'},
+                                   {'Name': 'Currency', 'Value': 'USD'}]
+                }
+            }, {
+                'timestamps': [datetime.datetime(2019, 4, 29, 12, 0), datetime.datetime(2019, 4, 30, 0, 0)],
+                'values': [174.96, 179.09],
+                'config': {
+                    'metric_name': 'EstimatedCharges',
+                    'dimensions': [{'Name': 'Currency', 'Value': 'USD'}]
+                }
+            }]
+        result = [
+            {
+                'service': 'AmazonCloudWatch',
+                'timestamps': [datetime.datetime(2019, 4, 29, 12, 0), datetime.datetime(2019, 4, 30, 0, 0)],
+                'values': [446.11, 458.22]
+            }, {
+                'service': 'Total',
+                'timestamps': [datetime.datetime(2019, 4, 29, 12, 0), datetime.datetime(2019, 4, 30, 0, 0)],
+                'values': [174.96, 179.09]
+            }
+        ]
         # mock準備
         mock_user = Mock()
         mock_aws = Mock()
-        mock_monitor_graph = Mock()
+        mock_start_time = Mock()
+        mock_end_time = Mock()
+        mock_period = Mock()
+        mock_stat = Mock()
 
         mock_user.can_fetch_billing.return_value = True
         mock_user.has_aws_env.return_value = True
 
-        get_chart = mock_cloudwatch.return_value.get_chart
-        res = ControlAwsEnvironment(Mock()).billing_graph(mock_user, mock_aws, mock_monitor_graph)
+        get_multi_charts = mock_cloudwatch.return_value.get_multi_charts
+        get_multi_charts.return_value = external_result
+        list_metrics = mock_cloudwatch.return_value.list_metrics
+        res = ControlAwsEnvironment(Mock()).billing_graph(mock_user, mock_aws, mock_start_time, mock_end_time,
+                                                          mock_period, mock_stat)
+        params = dict(
+            name_space='AWS/Billing',
+            period=mock_period,
+            stat=mock_stat,
+            start_time=mock_start_time,
+            end_time=mock_end_time,
+            metric_data_queries=[]
+        )
 
         mock_user.has_aws_env.assert_called()
         mock_user.can_fetch_billing.assert_called()
-        mock_cloudwatch.assert_called_once_with(mock_aws, 'us-east-1')
-        get_chart.assert_called_once_with(mock_monitor_graph)
-        self.assertEqual(res, get_chart.return_value)
+        mock_cloudwatch.assert_called_with(mock_aws, 'us-east-1')
+        list_metrics.assert_called_once_with('AWS/Billing', 'EstimatedCharges', [])
+        get_multi_charts.assert_called_once_with(**params)
+        self.assertEqual(res, result)
+
+    # 請求情報取得：正常系 空データ
+    @mock.patch('backend.usecases.control_aws_environment.CloudWatch')
+    def test_billing_graph(self, mock_cloudwatch: Mock):
+        # テスト用の引数準備
+        external_result = [{
+                'timestamps': [],
+                'values': [],
+                'config': {
+                    'metric_name': 'EstimatedCharges',
+                    'dimensions': [{'Name': 'ServiceName', 'Value': 'AmazonCloudWatch'},
+                                   {'Name': 'Currency', 'Value': 'USD'}]
+                }
+            }]
+        result = [
+            {
+                'service': 'AmazonCloudWatch',
+                'timestamps': [],
+                'values': []
+            }
+        ]
+        # mock準備
+        mock_user = Mock()
+        mock_aws = Mock()
+        mock_start_time = Mock()
+        mock_end_time = Mock()
+        mock_period = Mock()
+        mock_stat = Mock()
+
+        mock_user.can_fetch_billing.return_value = True
+        mock_user.has_aws_env.return_value = True
+
+        get_multi_charts = mock_cloudwatch.return_value.get_multi_charts
+        get_multi_charts.return_value = external_result
+        list_metrics = mock_cloudwatch.return_value.list_metrics
+        res = ControlAwsEnvironment(Mock()).billing_graph(mock_user, mock_aws, mock_start_time, mock_end_time,
+                                                          mock_period, mock_stat)
+        params = dict(
+            name_space='AWS/Billing',
+            period=mock_period,
+            stat=mock_stat,
+            start_time=mock_start_time,
+            end_time=mock_end_time,
+            metric_data_queries=[]
+        )
+
+        mock_user.has_aws_env.assert_called()
+        mock_user.can_fetch_billing.assert_called()
+        mock_cloudwatch.assert_called_with(mock_aws, 'us-east-1')
+        list_metrics.assert_called_once_with('AWS/Billing', 'EstimatedCharges', [])
+        get_multi_charts.assert_called_once_with(**params)
+        self.assertEqual(res, result)
+
 
     # 請求情報取得：使用できないAWSアカウントの場合
     def test_billing_graph_cant_use_aws(self):
         mock_user = Mock()
         mock_aws = Mock()
-        mock_monitor_graph = Mock()
+        mock_start_time = Mock()
+        mock_end_time = Mock()
+        mock_period = Mock()
+        mock_stat = Mock()
 
         mock_user.can_fetch_billing.return_value = True
         mock_user.has_aws_env.return_value = False
 
         with self.assertRaises(PermissionDenied):
-            ControlAwsEnvironment(Mock()).billing_graph(mock_user, mock_aws, mock_monitor_graph)
+            ControlAwsEnvironment(Mock()).billing_graph(mock_user, mock_aws, mock_start_time, mock_end_time,
+                                                        mock_period, mock_stat)
 
         mock_user.has_aws_env.assert_called()
         mock_user.can_fetch_billing.assert_not_called()
@@ -200,13 +301,17 @@ class ControlAwsEnvironmentTestCase(TestCase):
     def test_billing_graph_dont_have_permission(self):
         mock_user = Mock()
         mock_aws = Mock()
-        mock_monitor_graph = Mock()
+        mock_start_time = Mock()
+        mock_end_time = Mock()
+        mock_period = Mock()
+        mock_stat = Mock()
 
         mock_user.can_fetch_billing.return_value = False
         mock_user.has_aws_env.return_value = True
 
         with self.assertRaises(PermissionDenied):
-            ControlAwsEnvironment(Mock()).billing_graph(mock_user, mock_aws, mock_monitor_graph)
+            ControlAwsEnvironment(Mock()).billing_graph(mock_user, mock_aws, mock_start_time, mock_end_time,
+                                                        mock_period, mock_stat)
 
         mock_user.has_aws_env.assert_called()
         mock_user.can_fetch_billing.assert_called()
--- backend/usecases/control_aws_environment.py
+++ backend/usecases/control_aws_environment.py
@@ -2,7 +2,6 @@ from django.core.exceptions import PermissionDenied
 from backend.models import UserModel, AwsEnvironmentModel, TenantModel, OperationLogModel
 from backend.logger import NarukoLogging
 from backend.externals.iam import Iam
-from backend.models.monitor import MonitorGraph
 from backend.externals.cloudwatch import CloudWatch
 
 
@@ -68,26 +67,54 @@ class ControlAwsEnvironment:
         aws_environment.delete()
         self.logger.info("END: delete_aws_environment")
 
-    def billing_graph(self, request_user: UserModel, aws: AwsEnvironmentModel, monitor_graph: MonitorGraph):
+    def billing_graph(self, request_user: UserModel, aws: AwsEnvironmentModel, start_time, end_time, period, stat):
         self.logger.info("START: graph")
 
         # 使用できるAWSアカウントか
         if not request_user.has_aws_env(aws):
             raise PermissionDenied("request user can't use aws account. user_id: {}, aws_id: {}"
                                    .format(request_user.id, aws.id))
-        
+
         # 請求情報を取得する権限を持っているか
         if not request_user.can_fetch_billing():
             raise PermissionDenied("request user can't fetch aws_environments. id:{}".format(request_user.id))
 
+        # 請求情報のリストメトリクスを一覧で取得
+        metrics = CloudWatch(aws, 'us-east-1').list_metrics(
+            'AWS/Billing',
+            'EstimatedCharges',
+            []
+        )
         # APIの引数を充足
-        monitor_graph.service_name = 'AWS/Billing'
-        monitor_graph.dimensions.append(
-            dict(
-                Name='Currency',
-                Value='USD'
+        # ここは配列じゃなくて辞書型にして、どのサービスかわかるように！
+        metric_data_queries = []
+        for metric in metrics:
+            metric_data_queries.append(dict(
+                metric_name=metric['MetricName'],
+                dimensions=metric['Dimensions']
             ))
-        monitor_graph = CloudWatch(aws, 'us-east-1').get_chart(monitor_graph)
+        params = dict(
+            name_space='AWS/Billing',
+            period=period,
+            stat=stat,
+            start_time=start_time,
+            end_time=end_time,
+            metric_data_queries=metric_data_queries
+        )
+        monitor_graphs = CloudWatch(aws, 'us-east-1').get_multi_charts(**params)
 
         self.logger.info("END: graph")
-        return monitor_graph
+
+        # 成型を行う config内から情報を絞りこむ
+        # 総計はserviceがUSDとなるので、これをTotalに変換
+        def pick_service_name(dimensions):
+            return next((dimension['Value'] for dimension in dimensions
+                         if dimension['Name'] == 'ServiceName'), 'Total')
+        result = map(lambda graph: dict(
+            service=pick_service_name(graph['config']['dimensions']),
+            timestamps=graph['timestamps'],
+            values=graph['values']
+        ), monitor_graphs)
+
+        return list(result)
+
